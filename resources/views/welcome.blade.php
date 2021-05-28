ff784c3b2c6988ce2d2b09e19fb946a1b17fa496
sebastian <sebastian.riquelme1801@alumnos.ubiobio.cl>
2021-05-28 12:52:13 -0400

    adminlte fix

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::page')
+
+@section('title', 'Propietarios')
+
+@section('content_header')
+<h1>Propietarios</h1>
+
+@stop
+
+@section('content')
+
+@stop
+
+@section('css')
+<link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+
+
+
+@stopphp
