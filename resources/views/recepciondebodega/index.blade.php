ae85cd7aa9363d163836d514bf609f4155381c5b
sebastian <sebastian.riquelme1801@alumnos.ubiobio.cl>
2021-05-30 13:26:14 -0400

    fix

--- resources/views/recepciondebodega/index.blade.php
+++ resources/views/recepciondebodega/index.blade.php
@@ -1,11 +1,352 @@
+@extends('adminlte::page')
 
-    protected $table ='recurso_unidad_negocio';
-    protected $primaryKey = 'run_id';
+@section('title', 'Departamento')
 
-    public function scopeRecurso($query,$unidad_negocio)
-    {
-      if($unidad_negocio)
-      {
-          return $query->where('unn_id','like',$unidad_negocio);
-      }
-    }
+@section('content_header')
+<h1>Recepcion de bodega OP</h1>
+
+@stop
+
+@section('content')
+    <div class="card">   
+        <h4><b>Proveedores</b></h4>   
+    
+        <div class="form-check-inline col-4">
+            <div class="form-group">
+                <select name="" id="" class="form-control custom-select">
+                    
+                    <option value="">-- Todas --</option>
+                    @foreach ($lista_final as $p)            
+                    <option value="{{$p['codigo']}}">{{$p['descripcion']}}</option>
+                    @endforeach            
+                    
+                </select>     
+            </div>  
+        </div>
+    </div>
+
+    <div class="form-group">
+
+        <select name="" id="" class="form-control custom-select">
+            
+            <option value="">--Selecione un proveedor --</option>
+            @foreach ($lista_final as $p)            
+             <option value="{{$p['codigo']}}">{{$p['descripcion']}}</option>
+            @endforeach            
+            
+            <h4><b>Empresa</b></h4>
+        
+            <div class="form-group col-4">
+                <select name="" id="empresa" class="form-control custom-select">                    
+                    <option value="">-- Todas --</option>                              
+                    
+                </select>     
+            </div>  
+
+
+            <h4><b>Unidad de negocio</b></h4>
+        
+            <div class="form-group col-4">
+                <select name="" id="unidadN" class="form-control custom-select">                    
+                    <option value="">-- Todas --</option>                              
+                    
+                </select>     
+            </div>  
+        
+            <div class="form-check-inline col-lg-6">   
+                <div class="form-group">
+                    <div class="form-check">
+                        <h4><b>Centro de costos</b></h4>
+                        <select name="" id="centroC" class="form-control custom-select">                    
+                            <option value="">-- Todas --</option>                              
+                            
+                        </select> 
+                    </div>
+                    <div class="form-check">
+                        <h4><b>Actividad</b></h4>
+                        <select name="" id="actividad" class="form-control custom-select">                    
+                            <option value="">-- Todas --</option>                              
+                            
+                        </select>   
+                    </div>                    
+                </div>
+
+                <div class="form-group">
+                    <div class="form-check">
+                        <h4><b>Recursos</b></h4>
+                        <select name="" id="recursos" class="form-control custom-select">                    
+                            <option value="">-- Todas --</option>                              
+                            
+                        </select> 
+                    </div>
+                    <div class="form-check">
+                        <h4><b>Unidad de medida</b></h4>
+                        <select name="" id="unidadM" class="form-control custom-select">                    
+                            <option value="">-- Todas --</option>                              
+                            
+                        </select>   
+                    </div>                    
+                </div>
+            </div>
+
+            
+        
+    </div>
+
+
+<br>
+<div class="card">
+    <div class="card-header">
+        {{--  Lista de radios  --}}
+       <center>
+        
+            
+            <div class="form-check-inline col-2">
+                
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                </div>
+            </div> 
+
+            <div class="form-check-inline col-2">   
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-check-inline col-2">    
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-check-inline col-2">    
+                <div class="form-group">
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="radio1" checked="">
+                        <label class="form-check-label">Radio checked</label>
+                    </div>
+                </div>
+            </div>
+
+        
+    </center>
+
+    </div>
+    <!-- /.card-header -->
+
+    <div class="card-header">
+        {{-- Segunda Lista de radios  --}}
+                 
+        <center>
+
+            <div class="form-group">
+                <div class="form-check-inline col-2">
+                    <input class="form-check-input" type="radio" name="radio2" checked="">
+                    <label class="form-check-label">Radio checked</label>
+                </div>
+                <div class="form-check-inline col-2">
+                    <input class="form-check-input" type="radio" name="radio2" checked="">
+                    <label class="form-check-label">Radio checked</label>
+                </div>
+                <div class="form-check-inline col-2">
+                    <input class="form-check-input" type="radio" name="radio2" checked="">
+                    <label class="form-check-label">Radio checked</label>
+                </div>
+            </div>           
+
+        </center>
+
+
+    </div>
+    <!-- /.card-header -->
+
+    <div class="card">
+        {{-- Buscadores  --}}
+        <br>
+        
+        <div class="form-row">
+            <div class="col-1"></div>
+            <div class="col-md-5 mb-3">
+              <h5><b>Texto para buscar:</b></h5>
+              <input type="text" class="form-control" id="">
+            </div>
+            <div class="col-md-4 mb-3">
+              <h5><b>Nº OP:</b></h5>
+              <input type="text" class="form-control" id="">
+            </div>            
+        </div>
+        
+        <button type="button" class="btn btn-block btn-primary">Buscar</button>
+
+    </div>
+    <!-- /.card-header -->
+    <div class="card">
+
+        <div class="card-success">
+            <div class="card-header table-responsive p-0">
+                <table class="table table-fixed text-nowrap" id="tabla1">
+                    <thead>
+                        <tr>
+                            <th>Fecha</th>
+                            <th>Numero OP</th>
+                            <th>Nombre algo</th>
+                            <th>Numero OP</th>
+                            <th>asdasd</th>
+                            <th>hgjghj</th>
+                            <th>hjkhjk</th>
+                            <th>werwerwer</th>                            
+                        </tr>
+                    </thead>
+                </table>
+            </div>            
+        </div>
+        
+        <div class="card-body table-responsive p-0" style="height: 300px;">
+            <table class="table table-fixed text-nowrap" id="tabla1">
+                <tbody>
+                    {{--  cosas dentro de la lista  --}}
+
+                </tbody>
+            </table>
+        </div>        
+    </div>
+    <!-- /.card-header -->
+
+    <button type="button" class="btn btn-block btn-success">Success</button>
+    <button type="button" class="btn btn-block btn-danger">Danger</button>
+
+</div>
+
+@stop
+
+@section('footer')
+
+<footer class="blockquote-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.1.0
+    </div>
+    <strong>Copyright © 2014-2021 <a href="https://adminlte.io/%22%3EAdminLTE.io</a>.</strong> All rights reserved".
+  </footer>
+
+
+
+@stop
+
+@section('css')
+<style> <?php include public_path('adminlte.css'); ?> </style>     
+
+@stop           
+
+@section('js')
+<script>
+    $(document).ready(function(){
+
+        $('#empresa').on('change',function(){
+            var empresa_id = $(this).val();
+            if ($.trim(empresa_id) != ''){
+                $.get('unidadN',{empresa_id: empresa_id},function(unidadN){
+                    $('#unidadN').empty();
+                    $('#unidadN').append("<option value=''>-- Todas --</option>");
+                    $.each(unidadN, function(index, value){
+                        $('#unidadN').append("<option value='"+ index +"'>"+ value +"</option>");
+                    })
+                });
+            }
+        });
+
+        $('#unidadN').on('change',function(){
+            var unidadN_id = $(this).val();
+            if ($.trim(unidadN_id) != ''){
+                $.get('centroC',{unidadN_id: unidadN_id},function(centroC){
+                    $('#centroC').empty();
+                    $('#centroC').append("<option value=''>-- Todas --</option>");
+                    $.each(centroC, function(index, value){
+                        $('#centroC').append("<option value='"+ index +"'>"+ value +"</option>");
+                    })
+                });
+
+                $.get('recursos',{unidadN_id: unidadN_id},function(recursos){
+                    $('#recursos').empty();
+                    $('#recursos').append("<option value=''>-- Todas --</option>");
+                    $.each(recursos, function(index, value){
+                        $('#recursos').append("<option value='"+ index +"'>"+ value +"</option>");
+                    })
+                });
+            }
+        });
+
+        $('#centroC').on('change',function(){
+            var centroC_id = $(this).val();
+            if ($.trim(centroC_id) != ''){
+                $.get('actividad',{centroC_id: centroC_id},function(actividad){
+                    $('#actividad').empty();
+                    $('#actividad').append("<option value=''>-- Todas --</option>");
+                    $.each(actividad, function(index, value){
+                        $('#actividad').append("<option value='"+ index +"'>"+ value +"</option>");
+                    })
+                });
+            }
+        });
+
+        $('#recursos').on('change',function(){
+            var recursos_id = $(this).val();
+            if ($.trim(recursos_id) != ''){
+                $.get('unidadM',{recursos_id: recursos_id},function(unidadM){
+                    $('#unidadM').empty();
+                    $('#unidadM').append("<option value=''>-- Todas --</option>");
+                    $.each(unidadM, function(index, value){
+                        $('#unidadM').append("<option value='"+ index +"'>"+ value +"</option>");
+                    })
+                });
+            }
+        });
+
+    });
+
+@stop
