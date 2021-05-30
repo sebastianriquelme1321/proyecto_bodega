e37f31cc5444ce291e5f4d8e234da54ced4ae850
sebastian <sebastian.riquelme1801@alumnos.ubiobio.cl>
2021-05-30 13:40:16 -0400

    scopes

--- app/recurso.php
+++ app/recurso.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class recurso extends Model
 {
     //
+
+    protected $table ='recurso';
+    protected $primaryKey = 'rec_id';
+
+    public function scopeRecurso($query,$recurso_unidad_negocio)
+    {
+      if($recurso_unidad_negocio)
+      {
+          return $query->where('rec_id','like',$recurso_unidad_negocio);
+      }
+    }
 }
